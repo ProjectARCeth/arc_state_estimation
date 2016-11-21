e9b77761aa756be99d6c1d6021fc16a8d3d315a9
Sele <s-h-schaefer@web.de>
2016-11-21 21:53:01 +0100

    changed

--- src/attitude_filter.cpp
+++ src/attitude_filter.cpp
@@ -16,13 +16,12 @@ ros::Publisher pose_pub;
 void kalman_updater(const sensor_msgs::Imu::ConstPtr & imu_data);
 
 //Definition of errors
-const float error_state_euler_dot = 40;
-const float error_state_linear_acceleration = 10;
-const float error_measurement_linear_accelerometer = 1;
-const float error_measurement_gyro = 0.001;
+const float error_state_euler_dot = 40 * 0.0001;
+const float error_state_linear_acceleration = 10 * 0.001;
+const float error_measurement_linear_accelerometer = 1 * 0.001;
+const float error_measurement_gyro = 0.001 * 0.001;
 
-//Initialising inital state
-const Eigen::VectorXd x0 = Eigen::VectorXd::Zero(9,9);
+
 
 //Declaration Kalman Filter
 KalmanFilter_Orientation kalman;
@@ -32,17 +31,25 @@ int main(int argc, char** argv){
 	ros::init(argc, argv, "attitudefilter");
 	ros::NodeHandle node;
 
+	//Initialising inital state
+	Eigen::VectorXd x_0(15);
+	x_0[0]= 10.0; x_0[1]=0.0; x_0[2]=0.0;	//positions
+	x_0[3]= 0.0; x_0[4]=0.0; x_0[5]=0.0;	//euler angles
+	x_0[6]= 0.0; x_0[7]=0.0; x_0[8]=0.0;	//linear velocities
+	x_0[9]= 0.0; x_0[10]=0.0; x_0[11]=0.0;	//angular velocities
+	x_0[12]= 0.0; x_0[13]=0.0; x_0[14]=0.0;	//linear accelerations
 	
 	//Initialising Kalman Filter
-	kalman.init_with_errors(x0, error_state_euler_dot, error_state_linear_acceleration,
+	kalman.init_with_errors(x_0, 
+					error_state_euler_dot, error_state_linear_acceleration,
                     error_measurement_gyro, error_measurement_linear_accelerometer);
 
 	//Subscribing & Update loop
 	while(ros::ok()){
 
 		imu_sub = node.subscribe("/imu0", 1, kalman_updater);
-		orientation_pub = node.advertise<geometry_msgs::Vector3>("current_euler", 1);
-		pose_pub = node.advertise<geometry_msgs::Pose>("/current_pose", 1);
+		orientation_pub = node.advertise<geometry_msgs::Vector3>("current_euler", 10);
+		pose_pub = node.advertise<geometry_msgs::Pose>("/current_pose", 10);
 
 		ros::spinOnce();
 	}

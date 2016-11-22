87ac5661718965a53fa07e160529b4b337dcdaa1
Sele <s-h-schaefer@web.de>
2016-11-22 18:04:47 +0100

    introducing params

--- src/attitude_filter.cpp
+++ src/attitude_filter.cpp
@@ -5,34 +5,31 @@
 #include <eigen3/Eigen/Eigen>
 
 #include "ros/ros.h"
-#include "geometry_msgs/Vector3.h"
 #include "geometry_msgs/Quaternion.h"
+#include "geometry_msgs/Vector3.h"
 #include "sensor_msgs/Imu.h"
 
-ros::Subscriber imu_sub;
-ros::Publisher orientation_pub;
-ros::Publisher pose_pub;
-
-void kalman_updater(const sensor_msgs::Imu::ConstPtr & imu_data);
-
+ros::Subscriber imuSub;
+ros::Publisher orientationPub;
+ros::Publisher posePub;
+//Declaration of functions
+void kalmanUpdater(const sensor_msgs::Imu::ConstPtr & imu_data);
+void getParameters(ros::NodeHandle& node);
 //Definition of errors
-const float error_state_euler_dot = 40 * 0.0001;
-const float error_state_linear_acceleration = 10 * 0.001;
-const float error_measurement_linear_accelerometer = 1 * 0.001;
-const float error_measurement_gyro = 0.001 * 0.001;
-
+double errorStateEulerDot;
+double errorStateLinearAcceleration;
+double errorMeasurementLinearAccelerometer;
+double errorMeasurementGyro;
 //Updating Queue ~ Updating Frequency
-int queue_length = 200;
-
-
+int queueLength;
 //Declaration Kalman Filter
-KalmanFilter_Orientation kalman;
+KalmanFilterOrientation kalman;
 
 int main(int argc, char** argv){
-
 	ros::init(argc, argv, "attitudefilter");
-	ros::NodeHandle node;
-
+	ros::NodeHandle node;		
+	//Getting parameters
+	getParameters(node);
 	//Initialising inital state
 	Eigen::VectorXd x_0(15);
 	x_0[0]= 10.0; x_0[1]=0.0; x_0[2]=0.0;	//positions
@@ -40,36 +37,36 @@ int main(int argc, char** argv){
 	x_0[6]= 0.0; x_0[7]=0.0; x_0[8]=0.0;	//linear velocities
 	x_0[9]= 0.0; x_0[10]=0.0; x_0[11]=0.0;	//angular velocities
 	x_0[12]= 0.0; x_0[13]=0.0; x_0[14]=0.0;	//linear accelerations
-	
 	//Initialising Kalman Filter
-	kalman.init_with_errors(x_0, 
-					error_state_euler_dot, error_state_linear_acceleration,
-                    error_measurement_gyro, error_measurement_linear_accelerometer);
-
+	kalman.initWithErrors(x_0, 
+					errorStateEulerDot, errorStateLinearAcceleration,
+                    errorMeasurementGyro, errorMeasurementLinearAccelerometer);
 	//Subscribing & Update loop
 	while(ros::ok()){
-
-		imu_sub = node.subscribe("/imu0", queue_length, kalman_updater);
-		orientation_pub = node.advertise<geometry_msgs::Vector3>("current_euler", queue_length);
-		pose_pub = node.advertise<geometry_msgs::Pose>("/current_pose", queue_length);
-
+		imuSub = node.subscribe("/imu0", queueLength, kalmanUpdater);
+		orientationPub = node.advertise<geometry_msgs::Vector3>("current_euler", queueLength);
+		posePub = node.advertise<geometry_msgs::Pose>("/current_pose", queueLength);
 		ros::spinOnce();
 	}
-
 	return 0;
 }
 
-
-void kalman_updater(const sensor_msgs::Imu::ConstPtr & imu_data){
-
+void kalmanUpdater(const sensor_msgs::Imu::ConstPtr & imuData){
 	//Updating kalman filter
-	kalman.update(imu_data);
-
+	kalman.update(imuData);
 	//Publishing euler angles & pose
-	orientation_pub.publish(kalman.get_angles());
-	pose_pub.publish(kalman.get_pose());
+	orientationPub.publish(kalman.getAngles());
+	posePub.publish(kalman.getPose());
 }
 
+void getParameters(ros::NodeHandle& node){
 
-
+	node.getParam("/StateEstimation/ImuFilter/errorStateEulerDot", errorStateEulerDot);
+	node.getParam("/StateEstimation/ImuFilter/errorStateLinearAcceleration", 
+		errorMeasurementLinearAccelerometer);
+	node.getParam("/StateEstimation/ImuFilter/errorMeasurementGyro", errorMeasurementGyro);
+	node.getParam("/StateEstimation/ImuFilter/errorMeasurementLinearAccelerometer", 
+		errorMeasurementLinearAccelerometer);
+	node.getParam("/StateEstimation/ImuFilter/subAndPubQueueLength", queueLength);
+}
 
